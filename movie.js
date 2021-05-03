f0ef92e404e6d192ebca0157e877fde5550cee40
arunasrivastava <aruna-sri@outlook.com>
2021-05-02 22:13:09 -0700

    final commits

--- movie.js
+++ movie.js
@@ -1,17 +1,18 @@
+function movieObject(title, rating) {
+    this.title = title;
+    this.rating = rating;
+    this.isValid = function () {
+      let x = document.getElementById("name").value;
+      if ((this.title !== null) && (this.rating >= 1) && (this.rating <= 5)){
+        return true
+      }
+      else{ 
+        return false;
+      }
+    };
+    
+    this.getAll = function() {
+      return "Movie: " + this.title   + ", Rating: " + this.rating;
+    };
+  }
   
-function movieObject(title, rating) 
-{
-  this.name = title,
-  this.rating = rating,
-  this.isValid = function() 
-  {
-    if (this.name != "" && this.rating >= 1 && this.rating <= 5)
-    {
-      return true;
-    }
-  };
-  this.getAll = function()
-  {
-    return this.name + " (" + this.rating + " out of 5)";
-  };
-}
